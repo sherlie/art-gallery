12e9e721b121a5f454f3b0086a6dc25e07572cd5
sherlie <dodoinwl@gmail.com>
2022-08-07 19:45:19 +0300

    Deploy-related configs

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,12 +1,15 @@
 import { Page, Artwork } from "./types";
 
+// for local dev use 'http://localhost:8080'
+const API_URL = 'https://artsy-gallery-api.herokuapp.com';
+
 export type FetchArtworksParams = {
   start: number;
   limit: number;
 }
 
 export const fetchArtworks = async ({ start, limit }: FetchArtworksParams): Promise<Page<Artwork>> => {
-  const response = await fetch(`http://localhost:8080/gallery?start=${start}&limit=${limit}`);
+  const response = await fetch(`${API_URL}/gallery?start=${start}&limit=${limit}`);
   const data = await response.json();
   return data;
 }
